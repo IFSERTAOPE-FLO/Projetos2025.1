3e3d492be61dd9613a184006516e2182373d2779
Sabrina Ferraz <162391279+sabrinafs29@users.noreply.github.com>
2025-06-25 14:07:37 -0300

    .

--- principal.php
+++ principal.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>Galeria Virtual</title>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600&display=swap" rel="stylesheet" />
-    <link rel="stylesheet" href="css/principal.css">
+    <link rel="stylesheet" href="css/principal.css?<?php echo time, ?>" >
     <link rel="shortcut icon" type="imagex/png" href="img/logo.png">
 </head>
 
@@ -37,21 +37,74 @@
                 <button class="aba" role="tab" aria-selected="false" aria-controls="phase4" id="tab-phase4"
                     tabindex="-1">Fase 4</button>
             </nav>
-            <article id="bio-geral" class="bio-content">
-                <p>xxxxxxxxxxxxxxxxxxxxxxxx</p>
-            </article>
-            <article id="phase1" class="bio-content" role="tabpanel" aria-labelledby="tab-phase1" hidden>
-                <p>fase 1</p>
+            <article id="bio-geral" class="bio-content" >
+                <p>No ano de 1944, a pequena cidade de Floresta do Navio, no interior de Pernambuco, serviu de berço
+                    para esta que se tornou uma ilustre representante dos artistas nordestinos.</p>
+                <p>Maria da Conceição de Souza Cahú deixou Floresta ainda jovem, foi para Recife estudar Belas Artes e,
+                    aos 25 anos, chegou a São Paulo.</p>
+                <p>Caricaturista, cartunista, retratista, quadrinista e ilustradora. Estas diferentes técnicas marcaram
+                    seu trabalho na capital paulista nos últimos 20 anos.
+                    Combatente e sensível, Cahú emprestou sua arte à luta política, ao movimento feminista, às questões
+                    sociais.</p>
+                <p> As imagens reproduzidas neste catálogo, selecionadas entre centenas de originais, foram organizadas
+                    respeitando a cronologia, em quatro fases:</p>
+                <ul>
+                    <li>Seus primeiros trabalhos em Floresta e Recife;</li>
+                    <li>A década de 70, marcada predominantemente por suas charges e caricaturas sobre o ambiente
+                        político;</li>
+                    <li>A década de 80, quando retrata a elite empresarial brasileira nas várias publicações da Gazeta
+                        Mercantil;</li>
+                    <li>Os anos 90, registro de sua maturidade e ousadia
+                        para enfrentar o óleo sobre tela.</li>
+                </ul>
             </article>
             <article id="phase2" class="bio-content" role="tabpanel" aria-labelledby="tab-phase2" hidden>
-                <p>fase 2</p>
+                <h4>Anos 70</h4>
+                <p>Com traço limpo, técnica apurada e aguda sensibilidade, ela mostra um pouco do seu esforço para
+                    colocar a sua arte a serviço das comunidades com que passou a conviver e a trabalhar desde que se
+                    mudou para a plúmbea paisagem paulistana. Deixou de lado empregos bem-remunerados e caiu na vida.
+                    Foi à luta, descobrindo, desta vez, que não estava só no seu sofrimento. Havia toda uma massa
+                    precisando, tanto quanto ela, de solidariedade.</p>
+                <p>Sentiu, com emoção confessada, que sua atte era uma arma poderosa e poderia ajudar muito a causa da
+                    sua gente. Ligou-se a sindicatos, a partidos políticos, a associações comunitárias, ao movimento de
+                    libertação das mulheres e, a partir de seu desempenho honesto e direito começou a aprender com o
+                    povo novas formas de expressão. Encheu folhetos, panfletos e cartazes defendendo direitos populares,
+                    que se misturam com naturalidade com os sonhos, fantasias e estandartes das principais organizações
+                    carnavalescas de São Paulo.</p>
             </article>
             <article id="phase3" class="bio-content" role="tabpanel" aria-labelledby="tab-phase3" hidden>
-                <p>fase 3</p>
+                <h4>Anos 80</h4>
+                <p>Nesta fase. Cahú já acumulava grande experiência com suas interferências na vida política e social do
+                    país.</p>
+                <p>Começa, então, a permear as informações económicas e a elite empresarial brasileira com imaginação,
+                    colorido e técnica.</p>
+                <p>A primeira ilustração colorida, feita para o Relatório Turista Sofisticado, da Gazeta Mercantil,
+                    abriu
+                    mais um espaço para a continuidade de seu trabalho, e assim tem sido nos últimos 15 anos.</p>
+                <p>Praticamente nenhum empresário escapou das mãos hábeis e talentosas de Maria da Conceição, a Cahú.
+                </p>
+            </article>
+            <article id="phase3" class="bio-content" role="tabpanel" aria-labelledby="tab-phase3" hidden>
+                <h4>Anos 80</h4>
+                <p>Nesta fase. Cahú já acumulava grande experiência com suas interferências na vida política e social do
+                    país.</p>
+                <p>Começa, então, a permear as informações económicas e a elite empresarial brasileira com imaginação,
+                    colorido e técnica.</p>
+                <p>A primeira ilustração colorida, feita para o Relatório Turista Sofisticado, da Gazeta Mercantil,
+                    abriu
+                    mais um espaço para a continuidade de seu trabalho, e assim tem sido nos últimos 15 anos.</p>
+                <p>Praticamente nenhum empresário escapou das mãos hábeis e talentosas de Maria da Conceição, a Cahú.
+                </p>
             </article>
             <article id="phase4" class="bio-content" role="tabpanel" aria-labelledby="tab-phase4" hidden>
-                <p>fase 4</p>
+                <h4>Anos 90</h4>
+                <p>O que se tem a destacar é o mistério que se arremessa de cada uma destas pinturas e das suas geometrias simples. São os milagres da arte. No exemplo de Conceição Cahú, “consistência” liga-se à consistência artesanal, que é toda a sua verdade, o ponto de união entre sensível e inteligível. </p>
+                <p>Começa, então, a permear as informações económicas e a elite empresarial brasileira com imaginação,
+                    colorido e técnica.</p>
+                <p>Os quadros de Conceição Cahú seriam reconhecidos como retratos da … humana se estivessem em Manaus ou Nova York. Com essa inexplicável capacidade que têm os artistas de serem intemporais no seu próprio tempo e serem do mundo nas suas aldeias. Conceição Cahú junta índios e civilização, mulheres e paisagens, Xingú e Capibaribe, como juntaria idealmente Manhattans e Rasos da Catarina, Volgas e seus Riachos do Navio.</p>
+                <p>Garibaldi Otávio, jornalista, Editor-sênior da Gazeta Mercantil de São Paulo</p>
             </article>
+
         </section>
 
         <?php
